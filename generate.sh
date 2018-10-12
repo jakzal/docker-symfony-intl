bd3d0789f115b7587163646b25cd484777ac38a3
Jakub Zalas <jakub@zalas.pl>
2018-10-12 12:32:34 +0100

    Move PHP 7.1 to the legacy-7.x branch

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PHP_VERSIONS=(7.0)
+PHP_VERSIONS=(7.0 7.1)
 ICU_RELEASES=(4.4.2 4.6.1 4.8.1.1 49.1.2 50.1.2 51.2 52.1 53.1 54.1 55.1 57.1 58.1 58.2 59.1 60.1 60.2 61.1 62.1)
 
 for icu_release in "${ICU_RELEASES[@]}"; do
